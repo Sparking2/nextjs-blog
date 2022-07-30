7d2734531cc5a5f97b09440a9e55200a7f454760
Sparking <sparking02@yahoo.com.mx>
2022-07-30 14:40:03 -0500

    test

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home({allPostsData}) {
                 <title>{siteTitle}</title>
             </Head>
             <section className={utilStyles.headingMd}>
-                <p>[Your Self introduction</p>
+                <p>[Your Self introduction man!]</p>
                 <p>
                     (This is a sample website - you’ll be building a site like this on{' '}
                     <a href={"https://nextjs.org/learn"}>our Next.js tutorial</a>
